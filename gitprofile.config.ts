b7b41acc1adac6d991a3cd029b3ece4465b0323f
Roberto Reale <85828358+RobertoReale@users.noreply.github.com>
2025-04-24 10:44:12 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -140,7 +140,7 @@ const CONFIG = {
   blog: {
     source: 'dev', // medium | dev
     username: 'robertoreale', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    limit: 1, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
